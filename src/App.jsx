3950a292488184c19343a5b5d6483f1239cc05a0
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-05-07 16:34:57 +0200

    header terminée et responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 // import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './components/layout/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <section className="bg-terracotta p-10">
-      <h1 className="text-center text-4xl font-bold">Seconde chance </h1>
-    </section>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
