ab793a09385a4760232052a24ee119f8f2f9bfba
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-05-07 10:48:28 +0200

    ajout des variables css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <section className="bg-test p-10">
+    <section className="bg-terracotta p-10">
       <h1 className="text-center text-4xl font-bold">Seconde chance </h1>
     </section>
   );
