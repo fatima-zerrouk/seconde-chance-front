28ef23c43d74bcd1b2c16b98c7bf5cc57b2a1a43
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-05-07 09:35:06 +0200

    ajout de dossiers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-function App() {  
+function App() {
   return (
     <section className="bg-test p-10">
       <h1 className="text-center text-4xl font-bold">Seconde chance </h1>
