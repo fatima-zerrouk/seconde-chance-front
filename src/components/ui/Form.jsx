ec8859bef1d6590593516b8e839b92d06e5088dc
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-07-02 16:10:25 +0200

    Texte alt des images 1, 2, 3 avec le nom de l'animal

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -22,6 +22,8 @@ export default function Form({ onSubmit, animalEdit }) {
     // Si animalEdit existe, RHF préremplit les champs tout seul
     defaultValues: animalEdit,
   });
+// watch() Regarde le champ name. Si il ne le trouves pas encore, il prends le nom de animalEdit, et si il n'y a rien, mets 'l'animal'
+const animalAltForm = watch('name', animalEdit?.name) || "l'animal";
 
   useEffect(() => {
     // Enregistre 'urls' comme un tableau requis
@@ -256,6 +258,7 @@ export default function Form({ onSubmit, animalEdit }) {
               key={index}
               index={index}
               currentUrl={currentUrls[index]}
+              animalAlt={animalAltForm} 
               onUploadSuccess={url => handleImageUploaded(index, url)}
               onRemove={() => handleImageRemoved(index)}
             />
