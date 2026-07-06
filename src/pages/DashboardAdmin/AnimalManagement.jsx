960bc8c98004efc6e04ba458e33fc166adc07961
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-07-06 14:44:47 +0200

    Ajout et correction des routes

---
+++ src/pages/DashboardAdmin/AnimalManagement.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { SectionAdmin } from '../../components/ui/Sections';
+
+export default function AnimalManagement() {
+  return <SectionAdmin title={'Gestion des animaux'}></SectionAdmin>;
+}
