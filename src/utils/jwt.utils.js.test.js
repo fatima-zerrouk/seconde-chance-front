5f34f3ab9be08e2bce63f852ff8a83ba80b43fb3
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-07-09 15:03:54 +0200

    Test composant simple card terminé

--- src/utils/jwt.utils.js.test.js
+++ src/utils/jwt.utils.js.test.js
@@ -1,8 +1,10 @@
 import { describe, it, expect } from 'vitest';
 import { isTokenValid } from './jwt.utils';
 
-const EXPIRED_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjM5MDIyfQ.1xB66pUyoYAFrdp1z685NxECOksD_oQ48mrHMuYh8M4';
-const VALID_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoyNTE2MjM5MDIyfQ.KZcWr-yh8K60LmITybQxUBETUcjQ4Vpuf17V7CWy8A0';
+const EXPIRED_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjM5MDIyfQ.1xB66pUyoYAFrdp1z685NxECOksD_oQ48mrHMuYh8M4';
+const VALID_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoyNTE2MjM5MDIyfQ.KZcWr-yh8K60LmITybQxUBETUcjQ4Vpuf17V7CWy8A0';
 
 describe('isTokenValid', () => {
   it('retourne false si le token est invalide', () => {
