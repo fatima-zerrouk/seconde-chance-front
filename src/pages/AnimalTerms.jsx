3ae58e016a4d51223486961de5f91e134b5815e5
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-07-21 23:08:47 +0200

    Page terminé

--- src/pages/AnimalTerms.jsx
+++ src/pages/AnimalTerms.jsx
@@ -3,6 +3,9 @@ import { LinkTerracota } from '../components/ui/Buttons';
 import OptimizedImage from '../components/ui/OptimizedImage';
 import { Helmet } from 'react-helmet-async';
 import { SectionSubtitles } from '../components/ui/Sections';
+import { CardRules } from '../components/ui/Cards';
+import { dataRules } from '../components/DataCards';
+import { HashLink } from 'react-router-hash-link';
 
 export default function AnimalTerms() {
   return (
@@ -19,13 +22,12 @@ export default function AnimalTerms() {
       <section className="relative flex flex-col justify-between  pb-16 lg:flex-row-reverse items-center bg-cream px-(--margin-mobile) md:lts md:ps-20">
         <OptimizedImage
           src="/assets/cats.png"
-          alt="Photo bannière d'un chien marron dans un drap"
+          alt="Photo bannière avec deux chatons roux"
           width="380"
           height="253"
           lazy={false}
           fetchPriority="high"
-          // className="w-full h-auto z-10"
-          className="relative z-10 w-full lg:transform-origin-bottom-left lg:scale-114 h-auto lg:-ml-5 lg:-mb-4"
+          className="relative z-10 w-full lg:transform-origin-bottom-left lg:scale-113 h-auto lg:-ml-5 lg:-mb-4"
         />
 
         <div className="relative">
@@ -49,17 +51,45 @@ export default function AnimalTerms() {
               ariaLabel={'Lien qui mène à la page du catalogue animal '}
               className="w-full md:w-60 mt-8 duration-500 hover:translate-x-5"
             />
-            <a
-              href="#adopt"
+            <HashLink
+              to="/#adopt"
               className="md:hover:z-30 w-full md:w-50  hover:translate-x-5 flex justify-center items-center mt-8 h-14 font-medium rounded-(--radius-button) border-2  hover:bg-brown hover:border-0 hover:text-lin transition duration-400 ease-in-out border-terracotta cursor-pointer"
             >
               Comment adopter
-            </a>
+            </HashLink>
           </div>
         </div>
       </section>
 
-      <SectionSubtitles></SectionSubtitles>
+      <SectionSubtitles
+        title={"Conditions d'adoption"}
+        paragraph={
+          'Adopter un animal est un acte  de respnsabilité. Voici ce que vous devez savoir avant de vous engager.'
+        }
+        className="text-center"
+      >
+        <div className="justify-items-center">
+          {dataRules.map(rule => (
+            <CardRules key={rule.id} {...rule} />
+          ))}
+
+          <CardRules
+            className="bg-lin"
+            title={'Avant de vous décider'}
+            text={
+              "Adopter un animal est une décision importante qui ne doit pas être prise à la légère. Prenez le temps de réfléchir et n'hésitez pas à nous contacter pour toute question. Notre équipe est là pour vous accompagner et vous aider à trouver le compagnon qui correspond vraiment à votre mode de vie."
+            }
+          />
+
+          <p className="mb-6">Prêt à donner une seconde chance à un animal ?</p>
+          <LinkTerracota
+            to={'/catalog'}
+            value={'Découvrez nos animaux'}
+            ariaLabel={'Lien qui mène à la page du catalogue animal '}
+            className="w-60 duration-500 hover:translate-x-5"
+          />
+        </div>
+      </SectionSubtitles>
     </>
   );
 }
